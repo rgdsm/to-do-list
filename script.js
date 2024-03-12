c953691c6b00f199f047ab04b526f2b828e26c1c
rgdsm <ryannngabriel94@gmail.com>
2024-03-11 21:59:57 -0300

    nova atualizacao

--- script.js
+++ script.js
@@ -2,20 +2,28 @@ let taskArea = document.querySelector(".taskArea")
 let addBtn = document.querySelector("#btnAdd")
 let inputText = document.querySelector("#inputText")
 
-let taskText = document.querySelector(".task")
+let taskText = document.querySelector(".taskArea .task")
 
 addBtn.addEventListener("click", function(btn){
 
-    btn.preventDefault()
+    btn.preventDefault();
 
     inputValue = inputText.value;
 
-    taskArea.style.display = "flex"
+    if (inputValue !== "") {
+
+        taskText.style.display = "block"
     
-    let newElement = document.createElement("div");
-    newElement.textContent = inputValue;
+        let newElement = document.createElement("li");
+    
+        newElement.classList.add("liActive")
+        newElement.classList.add("task")
+        newElement.textContent = inputValue;
+    
+        taskText.appendChild(newElement);
+    
+        inputText.value = "";
+}
 
-    taskArea.appendChild(newElement);
 
-    inputText.value = "";
 })
